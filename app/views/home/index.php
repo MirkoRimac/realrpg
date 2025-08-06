cb08753f3b6dd462bf24b050b3ddf88717554e21
Mirko Rimac <mirkorimac98@gmail.com>
2025-08-06 23:30:27 +0200

    Background Video on Hero-Element

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,14 @@
-<div class="home-header container-fluid text-center">
-    <h1>Willkommen bei RealRPG</h1>
+<div class="hero-video-container">
+  <video class="hero-video" autoplay muted loop playsinline>
+    <source src="sprites/hero-video.mp4" type="video/mp4">
+    Dein Browser unterstützt kein HTML5-Video.
+  </video>
+  <div class="overlay"></div>
+  <div class="hero-text">
+      <h2 class="fade-in-title" style="animation-delay: 0.5s; color:#0097B2;">Turn your life</h2>
+      <p class="fade-in-title" style="animation-delay: 1.5s; color:#FFDE59;">into a fantasy adventure</p>
+      <span class="fade-in-title" style="animation-delay: 2.5s;">with</span>
+      <img src="sprites/logo.png" class="fade-in-title" style="animation-delay: 3.5s; max-width: 250px;" alt="Logo">
+    <a class="btn btn-primary fade-in-title" style="animation-delay: 3.5s;" href="?controller=auth&action=register">Start Now</a>
+  </div>
 </div>
